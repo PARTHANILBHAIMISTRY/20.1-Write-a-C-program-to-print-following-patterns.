9ba0fbd8ede05a5d3295728e929ca7440755103b
PARTHANILBHAIMISTRY <75672237+PARTHANILBHAIMISTRY@users.noreply.github.com>
2021-02-23 23:38:19 +0530

    Add files via upload

---
+++ 20.1.c
@@ -0,0 +1,22 @@
+//20.1 Write a C program to print following patterns.
+//1
+//12
+//123
+//1234
+//12345
+#include <stdio.h>
+int main()
+{
+    int i, j, N;
+    printf("Enter N: ");
+    scanf("%d", &N);
+    for(i=1; i<=N; i++) 
+    { 
+     for(j=1; j<=i; j++) 
+        {
+        printf("%d", j);
+        }
+    printf("\n"); 
+    }
+    return 0;
+}
